735117fb66f0cb81e877756c018a488dd7a9a9c3
Sudi Omary <sudi.omary@rafikii.io>
2024-06-23 10:59:19 +0300

    CRU operations for knowledge base

--- src/main/java/com/gapco/backend/controller/KnowledgeBaseController.java
+++ src/main/java/com/gapco/backend/controller/KnowledgeBaseController.java
@@ -2,12 +2,11 @@ package com.gapco.backend.controller;
 
 
 import com.gapco.backend.dto.KnowledgeBaseCreateDTO;
-import com.gapco.backend.dto.TeamMemberCreateDTO;
-import com.gapco.backend.repository.KnowledgeBaseRepository;
 import com.gapco.backend.response.CustomApiResponse;
 import com.gapco.backend.service.KnowledgeBaseService;
 import com.gapco.backend.util.AppConstants;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -18,9 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -38,5 +35,47 @@ public class KnowledgeBaseController {
     public ResponseEntity<CustomApiResponse<Object>> AddKnowledgeBase(@Valid @RequestBody KnowledgeBaseCreateDTO knowledgeBaseCreateDTO){
         log.info("KnowledgeBaseController::AddKnowledgeBase Execution started");
         return new ResponseEntity<>(knowledgeBaseService.addNewKnowledgeBase(knowledgeBaseCreateDTO), HttpStatus.OK);
+    }
+
+
+    @Operation(
+            summary = "Get all knowledge bases"
+    )
+    @ApiResponse(responseCode = "200",content = { @Content(schema = @Schema(implementation = CustomApiResponse.class), mediaType = "application/json") })
+    @GetMapping()
+    public ResponseEntity<CustomApiResponse<Object>> getAll(
+            @Parameter(description = AppConstants.PAGE_NUMBER_DESCRIPTION) @RequestParam(defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
+            @Parameter(description = AppConstants.PAGE_SIZE_DESCRIPTION) @RequestParam(defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size,
+            @Parameter(description = AppConstants.SORT_BY_DESCRIPTION) @RequestParam(defaultValue = AppConstants.DEFAULT_SORT_BY) String sort,
+            @Parameter(description = AppConstants.SORT_DIRECTION_DESCRIPTION) @RequestParam(defaultValue = AppConstants.DEFAULT_SORT_DIRECTION) String dir
+    ){
+        log.info("KnowledgeBaseController::getAll Execution started");
+        return new ResponseEntity<>(knowledgeBaseService.getAll(page,size,sort,dir), HttpStatus.OK);
+    }
+
+    @Operation(
+            summary = "get blog details"
+    )
+    @ApiResponse(responseCode = "200",content = { @Content(schema = @Schema(implementation = CustomApiResponse.class), mediaType = "application/json") })
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomApiResponse<Object>> getDetails(
+            @Parameter(description = "Id of the knowledge") @PathVariable Long id
+    ){
+        log.info("KnowledgeBaseController::getDetails Execution started");
+        return new ResponseEntity<>(knowledgeBaseService.view(id), HttpStatus.OK);
+    }
+
+
+    @Operation(
+            summary = "update knowledge base",
+            description = "update knowledge base"
+    )
+    @ApiResponse(responseCode = "200",content = { @Content(schema = @Schema(implementation = CustomApiResponse.class), mediaType = "application/json") })
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomApiResponse<Object>> update(
+            @Parameter(description = "Id of the blog post") @PathVariable Long id, @RequestBody KnowledgeBaseCreateDTO knowledgeBaseCreateDTO
+    ){
+        log.info("KnowledgeBaseController::update Execution started");
+        return new ResponseEntity<>(knowledgeBaseService.update(id,knowledgeBaseCreateDTO), HttpStatus.OK);
     }
 }
--- src/main/java/com/gapco/backend/service/KnowledgeBaseService.java
+++ src/main/java/com/gapco/backend/service/KnowledgeBaseService.java
@@ -7,10 +7,16 @@ import com.gapco.backend.exception.EntityNotFoundException;
 import com.gapco.backend.repository.KnowledgeBaseRepository;
 import com.gapco.backend.repository.ServiceRepository;
 import com.gapco.backend.response.CustomApiResponse;
+import com.gapco.backend.util.AppConstants;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -45,6 +51,82 @@ public class KnowledgeBaseService {
 
             throw new EntityNotFoundException("Service with specified id is not found");
         }
+    }
+
+
+    public CustomApiResponse<Object> getAll(int page, int size, String sortBy, String sortDir) {
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+
+        Pageable pageable = PageRequest.of(page,size,sort);
+
+        Page<KnowledgeBase> pageableKnowledgeBases = knowledgeBaseRepository.findAll(pageable);
+
+        List<KnowledgeBase> knowledgeBases = pageableKnowledgeBases.getContent();
+
+        CustomApiResponse<Object> customApiResponse = new CustomApiResponse(
+                AppConstants.OPERATION_SUCCESSFULLY_MESSAGE,
+                pageableKnowledgeBases.getTotalElements(),
+                pageableKnowledgeBases.getTotalPages(),
+                pageableKnowledgeBases.getNumber()
+
+        );
+        customApiResponse.setData(knowledgeBases);
+        return customApiResponse;
+    }
+
+
+    public CustomApiResponse<Object> view(Long id) {
+
+        Optional<KnowledgeBase> checkKnowledgeBase = knowledgeBaseRepository.findById(id);
+
+        if(checkKnowledgeBase.isPresent()){
+
+            KnowledgeBase knowledgeDetails = checkKnowledgeBase.get();
+            CustomApiResponse<Object> customApiResponse = new CustomApiResponse<>("Record Founds");
+            customApiResponse.setData(knowledgeDetails);
+            return customApiResponse;
+
+        } else {
+            throw new EntityNotFoundException("knowledge not found");
+        }
+    }
+
+
+    public CustomApiResponse<Object> update(Long id,KnowledgeBaseCreateDTO blog) {
+
+        Optional<KnowledgeBase> findKnowledgeBase = knowledgeBaseRepository.findById(id);
+
+        if(findKnowledgeBase.isPresent()){
+
+            Optional<com.gapco.backend.entity.Service> serviceOptional = serviceRepository.findById(blog.getServiceId());
+
+            if(serviceOptional.isPresent()){
+                KnowledgeBase updatedKnowledge = findKnowledgeBase.get();
+                updatedKnowledge.setDescription(blog.getDescription());
+                updatedKnowledge.setTitle(blog.getTitle());
+
+                com.gapco.backend.entity.Service service = serviceOptional.get();
+
+                updatedKnowledge.setService(service);
+
+                knowledgeBaseRepository.save(updatedKnowledge);
+
+                CustomApiResponse<Object> customApiResponse = new CustomApiResponse<>("KnowledgeBase has been successfully updated");
+                customApiResponse.setData(updatedKnowledge);
+
+                log.info("KnowledgeBaseService::update Execution ended");
+                return customApiResponse;
+            } else {
+
+                throw new EntityNotFoundException("Service with specified id is not found");
+            }
+
+        } else {
+            throw new EntityNotFoundException("KnowledgeBase record is not found");
+        }
+
     }
 
 }
