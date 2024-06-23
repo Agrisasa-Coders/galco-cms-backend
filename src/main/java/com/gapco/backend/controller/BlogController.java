38bb41670bdbc300e89b28537e2280da9025c969
Sudi Omary <sudi.omary@rafikii.io>
2024-06-23 10:43:43 +0300

    CRU operations for blog post

--- src/main/java/com/gapco/backend/controller/BlogController.java
+++ src/main/java/com/gapco/backend/controller/BlogController.java
@@ -2,11 +2,13 @@ package com.gapco.backend.controller;
 
 
 import com.gapco.backend.dto.KnowledgeBaseCreateDTO;
+import com.gapco.backend.entity.Blog;
 import com.gapco.backend.response.CustomApiResponse;
 import com.gapco.backend.service.BlogService;
 import com.gapco.backend.service.KnowledgeBaseService;
 import com.gapco.backend.util.AppConstants;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -17,9 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -35,8 +35,49 @@ public class BlogController {
     )
     @ApiResponse(responseCode = "200",content = { @Content(schema = @Schema(implementation = CustomApiResponse.class), mediaType = "application/json") })
     @PostMapping
-    public ResponseEntity<CustomApiResponse<Object>> BlogController(@Valid @RequestBody KnowledgeBaseCreateDTO blog){
-        log.info("BlogController::BlogController Execution started");
+    public ResponseEntity<CustomApiResponse<Object>> AddBlogPost(@Valid @RequestBody KnowledgeBaseCreateDTO blog){
+        log.info("BlogController::AddBlogPost Execution started");
         return new ResponseEntity<>(blogService.addBlogPost(blog), HttpStatus.OK);
+    }
+
+
+    @Operation(
+            summary = "Get all blog posts"
+    )
+    @ApiResponse(responseCode = "200",content = { @Content(schema = @Schema(implementation = CustomApiResponse.class), mediaType = "application/json") })
+    @GetMapping()
+    public ResponseEntity<CustomApiResponse<Object>> getAll(
+            @Parameter(description = AppConstants.PAGE_NUMBER_DESCRIPTION) @RequestParam(defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
+            @Parameter(description = AppConstants.PAGE_SIZE_DESCRIPTION) @RequestParam(defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size,
+            @Parameter(description = AppConstants.SORT_BY_DESCRIPTION) @RequestParam(defaultValue = AppConstants.DEFAULT_SORT_BY) String sort,
+            @Parameter(description = AppConstants.SORT_DIRECTION_DESCRIPTION) @RequestParam(defaultValue = AppConstants.DEFAULT_SORT_DIRECTION) String dir
+    ){
+        log.info("BlogController::getAll Execution started");
+        return new ResponseEntity<>(blogService.getAll(page,size,sort,dir), HttpStatus.OK);
+    }
+
+    @Operation(
+            summary = "get blog details"
+    )
+    @ApiResponse(responseCode = "200",content = { @Content(schema = @Schema(implementation = CustomApiResponse.class), mediaType = "application/json") })
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomApiResponse<Object>> getDetails(
+            @Parameter(description = "Id of the blog") @PathVariable Long id
+    ){
+        log.info("BlogController::CompetitionController Execution started");
+        return new ResponseEntity<>(blogService.view(id), HttpStatus.OK);
+    }
+
+    @Operation(
+            summary = "update blog post",
+            description = "update blog post"
+    )
+    @ApiResponse(responseCode = "200",content = { @Content(schema = @Schema(implementation = CustomApiResponse.class), mediaType = "application/json") })
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomApiResponse<Object>> update(
+            @Parameter(description = "Id of the blog post") @PathVariable Long id, @RequestBody KnowledgeBaseCreateDTO blog
+    ){
+        log.info("BlogController::update Execution started");
+        return new ResponseEntity<>(blogService.update(id,blog), HttpStatus.OK);
     }
 }
--- src/main/java/com/gapco/backend/service/BlogService.java
+++ src/main/java/com/gapco/backend/service/BlogService.java
@@ -7,10 +7,26 @@ import com.gapco.backend.exception.EntityNotFoundException;
 import com.gapco.backend.repository.BlogRepository;
 import com.gapco.backend.repository.ServiceRepository;
 import com.gapco.backend.response.CustomApiResponse;
+import com.gapco.backend.util.AppConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -46,4 +62,80 @@ public class BlogService {
             throw new EntityNotFoundException("Service with specified id is not found");
         }
     }
+
+
+    public CustomApiResponse<Object> getAll(int page, int size, String sortBy, String sortDir) {
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+
+        Pageable pageable = PageRequest.of(page,size,sort);
+
+        Page<Blog> pageableBlogs = blogRepository.findAll(pageable);
+
+        List<Blog> blogs = pageableBlogs.getContent();
+
+        CustomApiResponse<Object> customApiResponse = new CustomApiResponse(
+                AppConstants.OPERATION_SUCCESSFULLY_MESSAGE,
+                pageableBlogs.getTotalElements(),
+                pageableBlogs.getTotalPages(),
+                pageableBlogs.getNumber()
+
+        );
+        customApiResponse.setData(blogs);
+        return customApiResponse;
+    }
+
+    public CustomApiResponse<Object> view(Long id) {
+
+        Optional<Blog> checkBlog = blogRepository.findById(id);
+
+        if(checkBlog.isPresent()){
+
+
+            Blog blogDetails = checkBlog.get();
+            CustomApiResponse<Object> customApiResponse = new CustomApiResponse<>("Record Founds");
+            customApiResponse.setData(blogDetails);
+            return customApiResponse;
+
+        } else {
+            throw new EntityNotFoundException("Blog Post not found");
+        }
+    }
+
+    public CustomApiResponse<Object> update(Long id,KnowledgeBaseCreateDTO blog) {
+
+        Optional<Blog> findBlog = blogRepository.findById(id);
+
+        if(findBlog.isPresent()){
+
+            Optional<com.gapco.backend.entity.Service> serviceOptional = serviceRepository.findById(blog.getServiceId());
+
+            if(serviceOptional.isPresent()){
+                Blog updatedBlogPost = findBlog.get();
+                updatedBlogPost.setDescription(blog.getDescription());
+                updatedBlogPost.setTitle(blog.getTitle());
+
+                com.gapco.backend.entity.Service service = serviceOptional.get();
+
+                updatedBlogPost.setService(service);
+
+                blogRepository.save(updatedBlogPost);
+
+                CustomApiResponse<Object> customApiResponse = new CustomApiResponse<>("Blog  post has been successfully updated");
+                customApiResponse.setData(updatedBlogPost);
+
+                log.info("BlogService::update Execution ended");
+                return customApiResponse;
+            } else {
+
+                throw new EntityNotFoundException("Service with specified id is not found");
+            }
+
+        } else {
+            throw new EntityNotFoundException("Blog Post not found");
+        }
+
+    }
+
 }
